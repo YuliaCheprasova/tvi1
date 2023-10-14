a130048dde5fc2e08183aacb717581d3681f9a26
YuliaCheprasova <cheprasovaja@yandex.ru>
2023-10-14 12:17:13 +0700

    6
    rastrigin's function is done and all are working

--- main.cpp
+++ main.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include <ctime>
 #include <algorithm>
+#include <cmath>
 
 using namespace std;
 
-int part(int p[], int in[], int start, int _end)
+int part(float p[], int in[], int start, int _end)
 {
-    int pivot = p[_end];
+    float pivot = p[_end];
     int pIndex = start;
     for (int i = start; i < _end; i++)
     {
@@ -21,7 +22,33 @@ int part(int p[], int in[], int start, int _end)
     swap (in[pIndex], in[_end]);
     return pIndex;
 }
-void quicksort(int p[], int in[], int start, int _end)
+void quicksort(float p[], int in[], int start, int _end)
+{
+    if (start >= _end) {
+        return;
+    }
+    int pivot = part(p, in, start, _end);
+    quicksort(p, in, start, pivot - 1);
+    quicksort(p, in, pivot + 1, _end);
+}
+int part(float p[], float in[], int start, int _end)
+{
+    float pivot = p[_end];
+    int pIndex = start;
+    for (int i = start; i < _end; i++)
+    {
+        if (p[i] <= pivot)
+        {
+            swap(p[i], p[pIndex]);
+            swap(in[i], in[pIndex]);
+            pIndex++;
+        }
+    }
+    swap (p[pIndex], p[_end]);
+    swap (in[pIndex], in[_end]);
+    return pIndex;
+}
+void quicksort(float p[], float in[], int start, int _end)
 {
     if (start >= _end) {
         return;
@@ -161,7 +188,7 @@ void cross(int fights, int m, int cross_switch, int **children, int **parents)
 }
 void mutation(string mut_switch, int n, int m, int **children)
 {
-    float mut, p_weak = 1./3.*m, p_average = 1./m, p_strong = 3./m;
+    float mut, p_weak = 1./(3.*m), p_average = 1./m, p_strong = 3./m;
     int i, j;
     if(mut_switch == "weak"){
 
@@ -216,12 +243,13 @@ void mutation(string mut_switch, int n, int m, int **children)
         }
 }
 
-void selection (string sel_switch, int *power, int fights, int n, int **parents, int **A, int m)
+void selection (string sel_switch, float *power, int fights, int n, int **parents, int **A, int m)
 {
-    int *temp_power = new int[n];
+    float *temp_power = new float[n];
+    float sumpower = 0;
     int *marks = new int[n];
     int *index = new int[n];
-    int fight, i, sumpower = 0, j, candidate1, candidate2, winner, cont = 0, k, sumrang = 0;
+    int fight, i, j, candidate1, candidate2, winner, cont = 0, k, sumrang = 0;
     float *rang = new float[n];
     float *p = new float[n];
     float temp = 0, randp;
@@ -411,17 +439,72 @@ void count_power2 (int n, int fights, int m, int *power, int **children)
         }
 }
 
+void rastr(float *x1, float *x2, float *power, bool parents, int n)
+{
+    for (int i = 0; i < n; i++)
+        {
+            if (parents)
+            power[i] = -(0.1*pow(x1[i],2)+0.1*pow(x2[i],2)-4*cos(0.8*x1[i])-4*cos(0.8*x2[i])+8);
+            else
+            power[i+n] = - (0.1*pow(x1[i+n],2)+0.1*pow(x2[i+n],2)-4*cos(0.8*x1[i+n])-4*cos(0.8*x2[i+n])+8);
+
+        }
+}
+
+void count_power(int **A, int n, int m, float h, int left, bool parents, float *x1, float *x2)
+{
+    int temp = 0, i, j, k;
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0, k = m/2-1; j < m/2; j++, k--)
+        {
+            if (A[i][j] == 1)
+            {
+                temp += pow(2,k);
+            }
+        }
+        if (parents)
+            x1[i] = h*temp + left;
+        else
+            x1[i+n] = h*temp + left;
+        temp = 0;
+        for (j = m/2, k = m/2-1; j < m; j++, k--)
+        {
+            if (A[i][j] == 1)
+            {
+                temp += pow(2,k);
+            }
+        }
+        if (parents)
+            x2[i] = h*temp + left;
+        else
+            x2[i+n] = h*temp + left;
+        temp = 0;
+    }
+}
+
+
 int main()
 {
 	setlocale(0, "");
 	srand(time(NULL));
-	string sel_switch = "rang";// prop, rang, tour
+	string sel_switch = "prop";// prop, rang, tour
 	int cross_switch = 1;//1, 2, 3
-	string mut_switch = "average";//weak, average, strong
-	int n = 250, m = 100, i, j, in, k, fights = n*2, globali, number_of_repeats = 500;
-	//candidate1, candidate2, winner, localpower = 0,
-	//splitter, splitter2, , p;
+	string mut_switch = "weak";//weak, average, strong
+	//int kind_of_func = 1;//1 - Растригина, 2 -
+	int n = 30, m = 0, s = 1, i, j, in, k, fights = n*2, globali, number_of_repeats = 80;
+	int left = -16, right = 16;
+    float e = 0.01, nparts, h;
 
+    nparts = (right - left)/e;
+    while (s < nparts + 1)
+    {
+        s *= 2;
+        m++;
+    }
+    h = (right - left)/(pow(2,m)-1);
+    cout << h << endl;
+    m *= 2;
 
 
 
@@ -431,8 +514,8 @@ int main()
     int** A2 = new int* [n];
 	for (i = 0; i < n; i++)
 		A2[i] = new int[m];
-    int *power1 = new int[n + fights];
-    int *power2 = new int[n];
+    float *power1 = new float[n*2];
+    float *power2 = new float[n];
     int** parents = new int*[fights];
 	for (i = 0; i < fights; i++)
 		parents[i] = new int[m];
@@ -440,47 +523,40 @@ int main()
 	for (i = 0; i < fights; i++)
 		children[i] = new int[m];
     int *index = new int[n + fights];
+    float *x1 = new float[n*2];
+    float *x2 = new float[n*2];
+    float *x12 = new float[n];
+    float *x22 = new float[n];
+
+
+
+
     //инициализация
     for (i = 0; i < n; i++)
 		for (j = 0; j < m; j++)
 			A1[i][j] = rand() % 2;
-	/*cout << "Population0" << endl;
+
+	count_power(A1, n, m, h, left, true, x1, x2);
+    rastr(x1, x2, power1, true, n);
+	cout << "Population0" << endl;
 	for (i = 0; i < n; i++)
-	{
-		for (j = 0; j < m; j++)
-		{
-			//cout.width(4);
-			cout << A1[i][j];
-		}
-		cout << endl;
-	}*/
+    {
+        cout << x1[i] <<" "<< x2[i] <<" "<< power1[i] << endl;
+    }
 	//глобальный цикл
 	for (globali = 1; globali < number_of_repeats; globali++)
     {
-        count_power1(n, m, power1, A1);
-        //for (i = 0; i < n; i++){
-           // cout << power[i]<<endl;
-        //}
-
         //селекция
         selection (sel_switch, power1, fights, n, parents, A1, m);
-        //cout_popul(100, parents, fights, m);
         //скрещивание
         cross(fights, m, cross_switch, children, parents);
-        //cout_popul(200, children, n, m);
         //мутация
         mutation(mut_switch, n, m, children);
-        //cout_popul(300, children, n, m);
-        count_power2(n, fights, m, power1, children);
-        //for (i = 0; i < fights; i++){
-            //cout << power[i]<<endl;
-        //}
-        for (i = 0; i < fights; i++)
+        count_power(children, n, m, h, left, false, x1, x2);
+        rastr(x1, x2, power1, false, n);
+        for (i = 0; i < n*2; i++)
             index[i] = i;
         quicksort(power1, index, 0, fights - 1);
-        //for (i = 0; i < fights; i++){
-            //cout << power[i]<<" "<<index[i]<<endl;
-        //}
         //создание новой популяции
         for (i = 0, k = fights-1; i < n; i++,k--)
         {
@@ -489,31 +565,39 @@ int main()
             {
                 for (j = 0; j < m; j++)
                     A2[i][j] = A1[in][j];
-                    power2[i] = power1[in];
+                power2[i] = power1[k];
+                x12[i] = x1[in];
+                x22[i] = x2[in];
             }
             if (in >= n)
             {
-                in -= n;
                  for (j = 0; j < m; j++)
-                    A2[i][j] = children [in][j];
-                    power2[i] = power1[in];
+                    A2[i][j] = children [in-n][j];
+                power2[i] = power1[k];
+                x12[i] = x1[in];
+                x22[i] = x2[in];
             }
         }
        for (i = 0; i < n; i++)
        {
+           power1[i] = power2[i];
+           x1[i] = x12[i];
+           x2[i] = x22[i];
            for (j=0; j < m; j++)
            {
                A1[i][j]=A2[i][j];
            }
        }
-       for (i = 0; i<n;i++)
-       {
-           power1[i] = power2[i];
-       }
-       cout_one(globali, A1, n, m);
+       cout << "Population" << globali << endl;
+       for (i = 0; i < n; i++)
+        {
+            cout << x1[i] <<" "<< x2[i] <<" "<< power1[i] << endl;
+        }
     }
 
 
+    delete[] x1;
+    delete[] x2;
     for (i = 0; i < n; i++)
 		delete [] A2[i];
 	delete[] A2;
@@ -530,4 +614,4 @@ int main()
 		delete [] A1[i];
 	delete[] A1;
 
-}
+}//заменить pow
